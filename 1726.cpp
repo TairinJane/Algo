1c129df1d207614d6f2a724aebc48eed747c3770
Tairin <tairinjane@gmail.com>
2020-02-22 18:00:47 +0300

    1726 accepted

--- 1726.cpp
+++ 1726.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
 
 using namespace std;
 
-int main() {
+int gosti() {
 
-    long int n;
+    long long int n;
     cin >> n;
-    long int x[n];
-    long int y[n];
+    long long int x[n];
+    long long int y[n];
     long long int sum = 0;
 
-    for (int i = 0; i < n; ++i) cin >> x[i] >> y[i];
+    for (long long int i = 0; i < n; ++i) cin >> x[i] >> y[i];
     sort(x, x + n);
     sort(y, y + n);
 
-    for (int i = 1; i < n; ++i) {
+    for (long long int i = 1; i < n; ++i) {
         sum += ((x[i] - x[i - 1]) + (y[i] - y[i - 1])) * i * (n - i) * 2;
     }
 
