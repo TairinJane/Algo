7d8f40e0d7cbc925ca535072ba955be1b93f14cb
Tairin <tairinjane@gmail.com>
2020-02-22 13:35:50 +0300

    1604 working with end problems

--- main.cpp
+++ main.cpp
@@ -4,10 +4,40 @@
 
 using namespace std;
 
+struct Sign {
+    int count;
+    int id;
+};
+
 int main() {
 
     int n;
     cin >> n;
+    Sign signs[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> signs[i].count;
+        signs[i].id = i + 1;
+    }
+
+    sort(signs, signs + n, [](Sign a, Sign b) { return a.count > b.count; });
+
+    int i = 0;
+    int j;
+    while (i < n) {
+        if (signs[i].count == 0) i++;
+        else {
+            cout << signs[i].id << " (i = " << i << ") ";
+            signs[i].count--;
+            j = n - 1;
+            while (signs[j].count == 0 && j > 0) j--;
+            if (signs[j].count != 0 && j >= 0) {
+                cout << signs[j].id << " (j = " << j << ") ";
+                signs[j].count--;
+            }
+            if (signs[i].count == 0) i++;
+        }
+    }
 
     return 0;
 }
