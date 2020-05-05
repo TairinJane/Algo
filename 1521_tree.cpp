fb9dbbaa6cecee156842ff620ad3a1500a081a88
Tairin <tairinjane@gmail.com>
2020-05-05 22:47:49 +0700

    1521 accepted

--- 1521_tree.cpp
+++ 1521_tree.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <cmath>
-#include <algorithm>
 
 using namespace std;
 
 int get_by_index(int tree[], int index, int border) {
     int cur = 1;
-    cout << "cur: " << cur << ", index: " << index << endl;
     while (cur * 2 < border) {
         if (tree[cur * 2] >= index) {
             cur *= 2;
@@ -14,7 +11,6 @@ int get_by_index(int tree[], int index, int border) {
             index -= tree[cur * 2];
             cur = cur * 2 + 1;
         }
-//        cout << "cur: " << cur << ", index: " << index << endl;
     }
     if (index == 1 && tree[2 * cur] > 0) return tree[2 * cur];
     return tree[2 * cur + 1];
@@ -32,15 +28,12 @@ int main() {
     int border = 1;
     while (border < n)
         border <<= 1;
-    cout << "border: " << border << endl;
     int tree[2 * border];
 
 
     for (int i = 0; i < n; i++)
         tree[border + i] = i + 1;
-    for (int i = border + n; i < 2 * border; ++i) {
-        tree[i] = 0;
-    }
+
     for (int i = border - 1; i > 0; i--) {
         if (2 * i < border) {
             tree[i] = tree[2 * i] + tree[2 * i + 1];
@@ -53,18 +46,13 @@ int main() {
         }
     }
 
-    for (int l = 1; l < border + n; ++l) {
-        cout << tree[l] << " ";
-    }
-    cout << endl;
-
     int cur = 1;
     while (n > 0) {
         cur = (cur + k - 1) % n;
         if (cur == 0) cur = n;
         n--;
         int index = get_by_index(tree, cur, border);
-        cout << index << endl;
+        cout << index << " ";
         tree[border + index - 1] = 0;
 
         int i = border + index - 1;
@@ -72,10 +60,6 @@ int main() {
             i /= 2;
             tree[i]--;
         }
-        /*for (int l = 1; l < 2 * border; ++l) {
-            cout << tree[l] << " ";
-        }
-        cout << endl;*/
     }
 
     return 0;
