ecae088ebb82f71390e96988d8d79071fdd11ec7
Tairin <tairinjane@gmail.com>
2020-05-06 16:41:30 +0700

    1628 with vector

--- main.cpp
+++ main.cpp
@@ -1,82 +1,110 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <vector>
+#include <set>
 
 using namespace std;
 
-int get_by_index(int tree[], int index, int border) {
-    int cur = 1;
-    cout << "cur: " << cur << ", index: " << index << endl;
-    while (cur * 2 < border) {
-        if (tree[cur * 2] >= index) {
-            cur *= 2;
-        } else {
-            index -= tree[cur * 2];
-            cur = cur * 2 + 1;
-        }
-//        cout << "cur: " << cur << ", index: " << index << endl;
-    }
-    if (index == 1 && tree[2 * cur] > 0) return tree[2 * cur];
-    return tree[2 * cur + 1];
-}
-
 int main() {
-    int n, k;
-    cin >> n >> k;
+    int m, n, k;
+    cin >> m >> n >> k;
 
-    if (n == 1) {
-        cout << 1;
-        return 0;
+    vector<pair<int, int>> points;
+    points.emplace_back(1, 0);
+    for (int i = 0; i < k; ++i) {
+        int a, b;
+        cin >> a >> b;
+        points.emplace_back(a, b);
+    }
+    points.emplace_back(m, n + 1);
+
+    sort(points.begin(), points.end(), [](pair<int, int> a, pair<int, int> b) {
+        return a.first == b.first ? a.second < b.second : a.first < b.first;
+    });
+
+    for (auto &point : points) {
+        cout << point.first << " " << point.second << endl;
     }
 
-    int border = 1;
-    while (border < n)
-        border <<= 1;
-    cout << "border: " << border << endl;
-    int tree[2 * border];
-
-
-    for (int i = 0; i < n; i++)
-        tree[border + i] = i + 1;
-    for (int i = border + n; i < 2 * border; ++i) {
-        tree[i] = 0;
-    }
-    for (int i = border - 1; i > 0; i--) {
-        if (2 * i < border) {
-            tree[i] = tree[2 * i] + tree[2 * i + 1];
+    int dj, di;
+    int count = 0;
+    int isolated_count = 0;
+    vector<pair<int, int>> isolated;
+    for (int i = 0; i < points.size() - 1; ++i) {
+        dj = points[i + 1].second - points[i].second;
+        di = points[i + 1].first - points[i].first;
+        cout << "di: " << di << ", dj: " << dj << endl;
+        if (di == 0) {
+            if (dj > 2) count++;
+            else if (dj == 2) isolated.emplace_back(points[i].first, points[i].second + 1);
         } else {
-            tree[i] = 0;
-            if (tree[2 * i] > 0)
-                tree[i]++;
-            if (tree[2 * i + 1] > 0)
-                tree[i]++;
+            int wall = n - points[i].second;
+            if (wall > 0) {
+                if (wall == 1) isolated.emplace_back(points[i].first, points[i].second + 1);
+                else count++;
+            }
+            if (points[i + 1].second > 1) {
+                if (points[i + 1].second == 2) isolated.emplace_back(points[i + 1].first, 1);
+                else count++;
+            }
+            if (di > 1) count += (di - 1);
         }
+        cout << "count by dj: " << count << endl;
+    }
+    cout << "count by dj: " << count << endl;
+
+    sort(points.begin(), points.end(), [](pair<int, int> a, pair<int, int> b) {
+        return a.second == b.second ? a.first < b.first : a.second < b.second;
+    });
+    points[0].first = 0;
+    points[0].second = 1;
+    points[k + 1].first = m + 1;
+    points[k + 1].second = n;
+
+    for (auto &point : points) {
+        cout << point.first << " " << point.second << endl;
     }
 
-    for (int l = 1; l < border + n; ++l) {
-        cout << tree[l] << " ";
-    }
-    cout << endl;
-
-    int cur = 1;
-    while (n > 0) {
-        cur = (cur + k - 1) % n;
-        if (cur == 0) cur = n;
-        n--;
-        int index = get_by_index(tree, cur, border);
-        cout << index << endl;
-        tree[border + index - 1] = 0;
-
-        int i = border + index - 1;
-        while (i > 1) {
-            i /= 2;
-            tree[i]--;
+    for (int i = 0; i < points.size() - 1; ++i) {
+        dj = points[i + 1].second - points[i].second;
+        di = points[i + 1].first - points[i].first;
+        cout << "di: " << di << ", dj: " << dj << endl;
+        if (dj == 0) {
+            if (di > 1) {
+                if (di == 2) isolated.emplace_back(points[i].first + 1, points[i].second);
+                else count++;
+            }
+        } else {
+            int wall = m - points[i].first;
+            if (wall > 0) {
+                if (wall == 1) isolated.emplace_back(points[i].first + 1, points[i].second);
+                else count++;
+            }
+            wall = points[i + 1].first - 1;
+            if (wall > 0) {
+                if (wall == 1) isolated.emplace_back(1, points[i + 1].second);
+                else count++;
+            }
+            if (dj > 1) count += (dj - 1);
         }
-        /*for (int l = 1; l < 2 * border; ++l) {
-            cout << tree[l] << " ";
-        }
-        cout << endl;*/
+        cout << "count by di: " << count << endl;
     }
+    cout << endl << "count: " << count << endl;
+
+    for (auto &point : isolated) {
+        cout << point.first << " " << point.second << endl;
+    }
+
+    set<pair<int, int>> unique(isolated.begin(), isolated.end());
+    cout << endl << "unique:" << endl;
+    for (auto &point : unique) {
+        cout << point.first << " " << point.second << endl;
+    }
+    count += (int) (isolated.size() - unique.size());
+
+    cout << endl << "count + isolated: " << count << endl;
+
 
     return 0;
 }
